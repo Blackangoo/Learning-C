c1d79fd88849f7b52ffeae5dfe2c6863e50a62b3
blackangoo <blackangoo.gc@gmail.com>
2023-07-11 14:31:28 +0200

    ex00 done

--- C-00/ex00/ft_putchar.c
+++ C-00/ex00/ft_putchar.c
@@ -1,1 +1,11 @@
-void ft_putchar(char c);
+#include <unistd.h>
+
+void ft_putchar(char c) /*My first function lol*/
+{
+    write(1, &c, 1);
+}
+
+int main(){
+    ft_putchar('x');
+    ft_putchar('\n');
+}
