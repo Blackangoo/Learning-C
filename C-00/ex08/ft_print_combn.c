3ed97a7a45b4e380470d300bf011363bbc1f5d5d
blackangoo <blackangoo.gc@gmail.com>
2023-07-12 21:29:45 +0200

    ex08 not finished

--- C-00/ex08/ft_print_combn.c
+++ C-00/ex08/ft_print_combn.c
@@ -5,23 +5,14 @@ void ft_putchar(char value)
 	write(1, &value, 1);
 }
 
-void print_from_n(int n)
-{
-    while (n < 10)
-    {
-        ft_putchar(n + '0');
-        n++;
-    }
-    
-}
-
 void ft_print_combn(int n)
 {
-    if (0 < n)
+    if (n > 0)
     {
+        0;
         ft_print_combn(n - 1);
-        print_from_n(n);
     }
+    
 }
 
 int main()
