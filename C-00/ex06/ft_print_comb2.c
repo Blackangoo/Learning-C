c83280e2fa1e2f54c860f16d30b14115cdc81fcc
blackangoo <blackangoo.gc@gmail.com>
2023-07-12 12:47:56 +0200

    ex06 done

--- C-00/ex06/ft_print_comb2.c
+++ C-00/ex06/ft_print_comb2.c
@@ -1,32 +1,49 @@
 #include <unistd.h>
 
+void ft_putchar(char value)
+{
+	write(1, &value, 1);
+}
+
+void ft_write(int x)
+{
+    int dizaine;
+    int unite;
+    if (x > 9)
+    {
+        dizaine = x / 10;
+        unite = x % 10;
+    }
+    else 
+    {
+        dizaine = 0;
+        unite = x;
+    }
+    ft_putchar('0' + dizaine);
+    ft_putchar('0' + unite);
+}
+
+void ft_print(int x, int y)
+{
+    ft_write(x);
+    ft_putchar(' ');
+    ft_write(y);
+    ft_putchar(',');
+    ft_putchar(' ');
+}
+
 void ft_print_comb2(void)
 {
-    int a = 48;
-    while (a < 58)
+    int x = 0;
+    while (x < 99)
     {
-        int b = 48;
-        while (b < 58)
+        int y = x + 1;
+        while (y < 100)
         {
-            int c = 48;
-            while (c < 58)
-            {
-                int d = c+1;
-                while (d < 58)
-                {
-                    write(1, &a, 1);
-                    write(1, &b, 1);
-                    write(1, " ", 1);
-                    write(1, &c, 1);
-                    write(1, &d, 1);
-                    write(1, ", ", 2);
-                    d++;
-                }
-                c++;
-            }
-            b++;
+            ft_print(x,y);
+            y++;
         }
-        a++;
+        x++;
     }
 }
 
