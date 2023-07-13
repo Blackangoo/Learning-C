d2b2d249025185809567750a08917a3441f34bdd
blackangoo <blackangoo.gc@gmail.com>
2023-07-13 22:59:27 +0200

    ex06 done

---
+++ C-02/ex06/ft_str_is_printable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ft_str_is_printable(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (str[i] < 32 || str[i] > 126)
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int main(void)
+{
+    char str[] = "Hello World !";
+    int ret = ft_str_is_printable(str);
+    printf("%d \n", ret);
+}
