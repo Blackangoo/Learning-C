7fa040ca126f7b9fa84e8c3c701fef81c93b1b28
blackangoo <blackangoo.gc@gmail.com>
2023-07-13 22:50:15 +0200

    ex05 done

--- C-02/ex05/ft_str_is_uppercase.c
+++ C-02/ex05/ft_str_is_uppercase.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int ft_str_is_lowercase(char *str)
+int ft_str_is_uppercase(char *str)
 {
     int i = 0;
     while (str[i])
@@ -19,6 +19,6 @@ int ft_str_is_lowercase(char *str)
 int main(void)
 {
     char str[] = "HELLOWORLD";
-    int ret = ft_str_is_lowercase(str);
+    int ret = ft_str_is_uppercase(str);
     printf("%d \n", ret);
 }
