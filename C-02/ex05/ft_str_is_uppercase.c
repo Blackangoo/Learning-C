40dae026a155cd0e16707482dae700fac2dec88b
blackangoo <blackangoo.gc@gmail.com>
2023-07-13 22:49:07 +0200

    ex05 done

---
+++ C-02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int ft_str_is_lowercase(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (str[i] < 'A' || str[i] > 'Z')
+        {
+            return 0;
+        }
+        
+        i++;
+    }
+    return 1;
+    
+}
+
+int main(void)
+{
+    char str[] = "HELLOWORLD";
+    int ret = ft_str_is_lowercase(str);
+    printf("%d \n", ret);
+}
