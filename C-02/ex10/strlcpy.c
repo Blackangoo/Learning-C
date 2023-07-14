60daaef06ba77be407debf1268dfb27aa28934c4
blackangoo <blackangoo.gc@gmail.com>
2023-07-14 12:21:18 +0200

    ex10 not done

---
+++ C-02/ex10/strlcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+    return;
+}
+
+int main(void)
+{
+    char src[] = "Je suis la source";
+    char dest[100];
+    unsigned int size = 20;
+}
