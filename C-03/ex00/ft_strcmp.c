3497bc91c65e58d613c19242a7d7f259326b260a
blackangoo <blackangoo.gc@gmail.com>
2023-07-21 12:41:39 +0200

    ex00 done

---
+++ C-03/ex00/ft_strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+    i = 0;
+    while ((s1[i] || s2[i]) && (s1[i]==s2[i]))
+    {
+        i++;
+    }
+    
+    return (s1[i] - s2[i]);
+}
+
+int main(void)
+{
+    char s1[] = "abcdef";
+    char s2[] = "ABCDEF";
+    printf("%d\n", ft_strcmp(s1, s2));
+	printf("%d\n", strcmp(s1, s2));
+    return 0;
+}
