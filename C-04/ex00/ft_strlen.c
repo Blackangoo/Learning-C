aba30d567f0dc07bb9e4be18ca6c9633c32d4301
blackangoo <blackangoo.gc@gmail.com>
2023-07-21 14:34:25 +0200

    ex00 done

---
+++ C-04/ex00/ft_strlen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return(i);
+}
+
+int main(void)
+{
+    char str[] = "Bonjour test test";
+
+    printf("%d \n", ft_strlen(str));
+
+    return(0);
+}
