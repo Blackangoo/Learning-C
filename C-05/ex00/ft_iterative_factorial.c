778d89ac1b9358291ba26cbf8b9bf11299c2a3d2
blackangoo <blackangoo.gc@gmail.com>
2023-07-28 17:21:13 +0200

    ex00 done

---
+++ C-05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb)
+{   
+    int res;
+    int i;
+
+    res = 1;
+    i = 1;
+    if (nb < 0)
+    {
+        return(0);
+    }
+    while (i <= nb)
+    {
+        res = res * i;
+        i++;
+    }
+    return(res);
+}
+
+int main(void)
+{
+    printf("%d \n", ft_iterative_factorial(4));
+}
