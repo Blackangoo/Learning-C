bab7af0bf31b3a1c2829265f8dfdfb14e88be826
blackangoo <blackangoo.gc@gmail.com>
2023-07-28 22:30:46 +0200

    ex05 done

---
+++ C-05/ex05/ft_sqrt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int sqr(int nb, int x)
+{
+    if (x > nb / 2)
+    {
+        return(0);
+    }
+    else if (nb / x == x)
+    {
+        return(x);
+    }
+    sqr(nb, x + 1);
+}
+
+int ft_sqrt(int nb)
+{
+    sqr(nb, 1);
+}
+
+int main(void)
+{
+    printf("%d \n", ft_sqrt(24));
+}
