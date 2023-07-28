8dda226de526c3b1cdbc0b751f185066af08c64a
blackangoo <blackangoo.gc@gmail.com>
2023-07-28 21:51:56 +0200

    ex01 done and corrected

--- C-05/ex01/ft_recursive_factorial.c
+++ C-05/ex01/ft_recursive_factorial.c
@@ -6,10 +6,14 @@ int ft_recursive_factorial(int nb)
     {
         return(1);
     }
+    else if (nb < 0)
+    {
+        return(0);
+    }
     return(nb * ft_recursive_factorial(nb - 1));
 }
 
 int main(void)
 {
-    printf("%d \n", ft_recursive_factorial(5));
+    printf("%d \n", ft_recursive_factorial(25));
 }
