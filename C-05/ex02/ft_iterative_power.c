b5485e0d2a5c06909d0835c5d83fd1aebc076a1c
blackangoo <blackangoo.gc@gmail.com>
2023-07-28 22:06:33 +0200

    ex02 done and corrected

--- C-05/ex02/ft_iterative_power.c
+++ C-05/ex02/ft_iterative_power.c
@@ -11,10 +11,6 @@ int ft_iterative_power(int nb, int power)
     {
         return(0);
     }
-    else if (nb == 0 && power == 0)
-    {
-        return(1);
-    }
     while (count <= power)
     {
         res = res * nb;
@@ -25,5 +21,5 @@ int ft_iterative_power(int nb, int power)
 
 int main(void)
 {
-    printf("%d \n", ft_iterative_power(5,3));
+    printf("%d \n", ft_iterative_power(0,0));
 }
