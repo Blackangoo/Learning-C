b2111729a3ba461f27e305c76465b3354e87a4db
blackangoo <blackangoo.gc@gmail.com>
2023-08-21 22:29:01 +0200

    ex13 of C12 done

---
+++ C-12/ex13/ft_list_merge.c
@@ -0,0 +1,12 @@
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_merge(t_list **begin_list1, t_list *begin_list2)
+{
+    t_list *list;
+
+    list = *begin_list1;
+    while (list->next)
+        list = list->next;
+    list->next = begin_list2;
+}
