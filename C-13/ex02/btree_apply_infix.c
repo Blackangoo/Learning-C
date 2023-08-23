8aa221c19ef732f5540ef7988ce13232cd9bec13
blackangoo <blackangoo.gc@gmail.com>
2023-08-23 15:14:21 +0200

    ex02 of C13 done

---
+++ C-13/ex02/btree_apply_infix.c
@@ -0,0 +1,13 @@
+#include "ft_btree.h"
+
+void btree_apply_infix(t_btree *root, void (*applyf)(void *))
+{
+    if (root)
+    {
+        btree_apply_prefix(root->left, applyf);
+        (*applyf)(root->item);
+        btree_apply_prefix(root->right, applyf);
+    }
+    else
+        return;
+}
